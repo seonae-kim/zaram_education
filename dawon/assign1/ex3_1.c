bedfa2f717d604561202c2b0c643bc7bbef4e243
dwlee331 <dawon@zaram.com>
2023-09-05 11:12:05 +0900

    ex3_1

--- dawon/assign1/ex3_1.c
+++ dawon/assign1/ex3_1.c
@@ -9,13 +9,13 @@ int main()
 	int n=0,k=0;
 	int i=0;
 	int p=0;
-	int b=1;
+	int b=0;
 
 	scanf("%d %d", &n, &k);
 
 	int *a;
 
-	a=(int*)malloc(sizeof(int) *n);
+	a=(int*)malloc(sizeof(int) *(n+1));
 
 	if(a==NULL)
 	{
@@ -23,17 +23,19 @@ int main()
 		exit(0);
 	}
 
-	printf("input: %d &d  ",n,k);
+	printf("input: %d %d  ",n,k);
 
 	for(i;i<n;i++)
 		*(a+i)=i+1;
+
+	printf("output: ");
 
 	while(1)
 	{
 		if(b==n)
 			break;
 
-		for(int j=1;j<=n;j++)
+		for(int j=0;j<n;j++)
 		{
 			if(*(a+j)==0)
 				continue;
@@ -42,15 +44,26 @@ int main()
 
 			if(p==k)
 			{
-				printf("output: %d :",*(a+j));
+				printf("%d ",*(a+j));
 				*(a+j)=0;
 				p=0;
 			}
 		}
 
 		b++;
+		if(b==n)
+		{	
+			for(int j=0;j<n;j++)
+			{
+				if(*(a+j) != 0)
+					printf("%d",*(a+j));
+			
+			}
 
+			break;
+		}
 	}
+	printf("\n");
 
 	free(a);
 	return 0;
