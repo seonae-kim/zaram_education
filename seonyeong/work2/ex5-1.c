892604d471419417cf456f98d43fd5fd4baeb8b4
seonyeong2 <sykwak@zaram.com>
2023-09-06 18:45:08 +0900

    ex5

--- seonyeong/work2/ex5-1.c
+++ seonyeong/work2/ex5-1.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 int main()
-{
-        int i = 0;
-        int j = 0;
-        int num[4][4] = { 0 };
-	int sum1_max[2] ={ 0 };
-	int sum2_max[2] ={ 0 };
-	int row[4] = { 0 };
-	int sum_total = 0;
+{       
+        int i = 0, j = 0, a = 0, b = 0;
+ 	int num[4][4] = { 0 };
+	int max = 0, sum = 0;
 
         printf("16 numbers: ");
 
@@ -25,63 +21,32 @@ int main()
         {
                 for (j = 0; j < 4; j++)
                 {
-                       if( num[0][i] + num[1][j] > sum1_max)
-		       { 
-			       sum1_max[1] = sum1_max[0];
-			       sum1_max[0] = num[0][i] + num[1][j];
-			       row[0] = i;
-			       row[1] = j;
-		       	 
-		       }
+			for (a = 0; a < 4; a++)
+               		{
+				for (b = 0; b < 4; b++)
+                       	 	{
+					if (j == i) continue;
+					if (j == a) continue;
+					if (a == b) continue;
+					
+					sum = num[0][i] + num[1][j] + num[2][a] + num[3][b];
+					if(sum>max)
+                                        {
+                                                max = sum;
+                                        }
+
+				}
+			}
 
                 }
         }
-			
+                                         printf("%d\n",max);
 
-	for(i = 0; i < 4; i++)
-        {
-                for (j = 0; j < 4; j++)
-                {
-                       if( num[2][i] + num[3][j] > sum2_max)
-                       { 
-			       sum2_max[1] = sum2_max[0];
-                               sum2_max[0] = num[2][i] + num[3][j];
-			       row[2] = i;
-			       row[3] = j;
-                       }
+        printf("\n");
 
-                }
-        }
- 
-	///////	
-        
-	if (row[1] == row[0])
-	{	
-		sum_total = sum1_max[1] + sum2_max[0];
-	        printf("%d ", sum_total);	
-	}
-
-	if(row[2] == row[1])
-	{
-		if(sum1_max[0]+sum2_max[1] > sum1_max[1]+sum2_max[0])
-		{
-			sum_total = sum1_max[0]+sum2_max[1];
-			printf("%d ", sum_total);
-
-		}
-		else 
-		{
-			sum_total = sum1_max[1]+sum2_max[0];
-			printf("%d ", sum_total);
-		}
-	}
-	else sum_total = sum1_max[0] + sum2_max[0];
-	printf("%d ", sum_total);
-	
-	printf("\n");
-	return 0;
 
 }
+
 
 
 
