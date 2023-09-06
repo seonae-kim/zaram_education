4f789b627ad97d113a6ba8036e956dfc951ea7b2
seonyeong2 <sykwak@zaram.com>
2023-09-06 16:08:04 +0900

    re-4

--- seonyeong/work2/4.c
+++ seonyeong/work2/4.c
@@ -2,50 +2,38 @@
 
 int main()
 {
-    int i;
-    int num[100];
+    int i, j;
+    int num[10]={ 0 };
+    int max =  0 ;
+    int new_arr[10] = { 0 };
+    int max_num = 0;
 
     printf("10 numbers:");
+   
 
     for (i = 0; i < 10; i++)
     {
         scanf("%d", &num[i]);
     }
-    int max_number = 0;
-    int min_number = 0;
-
-    printf("output:");
    
-
-    for (int p = 1; p < 10; p++)
+    for (i = 0; i < 10; i++)
     {
-        for (int i = 1; i < 10; i++)
-        {           
-            
-            if (num[i] > num[max_number])
-            { 
-                max_number = i;
-            }
-           
-            if (num[i] != 0 && num[i] < num[min_number])
-            {                
-                min_number = i;
-            }
-        } 
-        
-        if (p % 2 == 1)
+        for (j = 0; j < 10; j++)
         {
-            printf("%d ", num[max_number]);
-            num[max_number] = 0;
-        }
-        else
-        {
-            printf("%d ", num[min_number]); //min ?
-            num[min_number] = 0;
-        }      
+            if (num[j] >max)
+            {  
+                max = num[j];
+                max_num = j;
+            }
+        }       
+        new_arr[i] = max;
+        num[max_num] = 0;
+        max = 0;
+    }
         
-    }   
-
+    printf("%d %d %d %d %d %d %d %d %d %d ", new_arr[0], new_arr[9], new_arr[1], new_arr[8], 
+        new_arr[2], new_arr[7], new_arr[3], new_arr[6], new_arr[4],  new_arr[5] );
+    printf("\n");
     return 0;
 }
 
