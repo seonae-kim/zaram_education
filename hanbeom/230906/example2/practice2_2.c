1a8c1f8c0395b45fae8c83d92bbb5d1d5255e615
hanbeomm <hanbeom@zaram.com>
2023-09-06 17:28:20 +0900

    230906(3)

--- hanbeom/230906/example2/practice2_2.c
+++ hanbeom/230906/example2/practice2_2.c
@@ -21,22 +21,15 @@ int main() {
 	for(i =0 ;i < N;i++) {
 
 		scanf("%d", &number[i]);
-				
 	}
 
-
-	for (i =0; i < N/2; i++)    
+	for (i =0; i < N; i++)    
 	{
-		sub_number[i] = number[N - i -1]; 
-		printf("%d ", sub_number[i]);    
+		sub_number[i] = number[N - i -1];     
+		printf("%d ", sub_number[i]);
 	}
 
-	for (i = N/2; i <N; i++)
-	{
-		number[i] = number[N - i -1];
-		printf("%d ", number[i]);
 
-	}
 }
 
 
