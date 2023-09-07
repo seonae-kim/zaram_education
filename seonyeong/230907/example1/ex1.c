359a6aaef60d130559ddc281b3c06680b5aab44a
seonyeong2 <sykwak@zaram.com>
2023-09-07 17:46:10 +0900

    ex1

--- seonyeong/230907/example1/ex1.c
+++ seonyeong/230907/example1/ex1.c
@@ -1,113 +1,110 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 #include <string.h>
 
 
-int add(int a, int b)
+double add(double a, double b)
 {
-	double c = a + b;
-	return c;
+     printf("%.3f\n", a + b);
+	
 }
 
 
-int sub(int a, int b)
+double sub(double a, double b)
 {
-	double c = a - b;
-	return c;
+     printf("%.3f\n", a - b);
 }
 
 
-int mul(int a, int b)
+double mul(double a, double b)
 {
-	double c = a * b;
-	return c;
+    printf("%.3f\n", a * b);
 }
 
 
-int div(int a, int b)
+double div(double a, double b)
 {
-	double c = a / b;
-	return c;
+    printf("%.3f\n", a / b);
 }
 
-int main()
+double main()
 {
-   
+
 	int i = 0, j = 0;
 	char cal[20] = {0};
-	double cal_num1 = 0;
-	double cal_num2 = 0;
-	int op = 0;
-	int length = 0;
-	double result = 0;
+	char num1[20] = {0};
+	char num2[20] = {0};
+	char op ;		
+	int space1 = 0, space2 = 0;
+	double cal_num1 = 0, cal_num2 = 0;
 
 	printf("input expression: " );
 	scanf("%s", &cal);
 
-	length = strlen(cal);
 
-	
-	
-	//if (cal[0] == '-') //minus input
-	//{
-	//    for(i = 0; cal[i]; i++)
-	//    {
 
-	  //  }	
-	//}
-	
-
+    while(1)
+    {
+	    
 	for (i = 0; cal[i]; i++)
 	{
-	   if ((cal[i] = '+')||(cal[i] = '-')||(cal[i] = '*')||(cal[i] = '/'))
-	   {	       
-	       op = i;
-	        for ( j = 0; j < op ; j++ )
-		 {
-		     cal_num1 += (cal[j]-'0') * pow(10, (op -1 - j));
-		  }
-		for ( j = op + 1; j < length; j++ )
-		 {
-		     cal_num2 += (cal[j]-'0') * pow(10, (length - 1 - j)) ;
-	  	 }
-	   }
-	   //operation
-	    if (cal[op] = ( '+' ))
-            {
-               double result = add(cal_num1, cal_num2 );
-	       printf("%.2f", result);
-               break;
-
-            }
-            if (cal[op] = ( '-' ))
-            {
-                double result =  sub(cal_num1, cal_num2 );
-		printf("%.2f", result);
-                break;
-            }
-            if (cal[op] = ( '*' )) //conflict
-            {
-                double result =  mul(cal_num1, cal_num2 );//conflict
-		printf("%.2f", result);
-                break;
-            }
-            if (cal[op] = ( '/' ))
-            {
-                double result =  div(cal_num1, cal_num2 );
-		printf("%.2f", result);
-                break;
-            }
-	   
-
-	    if(cal[i] == 'q')
+	    if(cal[i] = 'q')
 	    {
                 printf("end");
+		break;
             }
 
-	    break;
-	} 
- 
+	    if (cal[i] = ' ')
+	    {
+		space1 = i;
+		for (j = 0; j < space1; j++)
+                {
+                    num1[j] = cal[j];		    
+                }
+		continue;
+	    }
+	    
+	    if ((cal[i] = '+')||(cal[i] = '-')||(cal[i] = '*')||(cal[i] = '/'))
+	    {
+		op = cal[i];	
+		continue;
+	    }	
 	   
+	    if (cal[i] = ' ')
+            {
+		space2 = i;
+                for (j = space2 ; cal[i] ; j++)
+                {
+                    num2[j] = cal[j];
+                }                
+            }
+	   
+	}
+	
+
+	cal_num1 = atof(num1);
+	cal_num2 = atof(num2);
+	
+	if (cal[op] = ( '+' ))
+	{
+	    add(cal_num1, cal_num2);
+	}
+	if (cal[op] = ( '-' ))
+        {
+            sub(cal_num1, cal_num2);
+        }
+	if (cal[op] = ( '*' ))
+        {
+            mul(cal_num1, cal_num2);
+        }
+	if (cal[op] = ( '/' ))
+        {
+            div(cal_num1, cal_num2);
+	}  
+	   	   
+
+	    
+    }	 
 	
  
 	return 0;	
