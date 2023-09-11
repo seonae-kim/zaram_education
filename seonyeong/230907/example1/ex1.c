87431c537785ecb40279213891810a0c3b6dfcfa
seonyeong2 <sykwak@zaram.com>
2023-09-11 11:04:18 +0900

    ex1_1

--- seonyeong/230907/example1/ex1.c
+++ seonyeong/230907/example1/ex1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 
 float add(float a, float b)
@@ -35,26 +36,35 @@ int main()
     char num2[20] = {0};
     char op ;
     float result = 0;
-    int count = 0;
+    int count = 0, p = 0;
     float cal_num1 = 0, cal_num2 = 0;
     int length = 0;
 
-    printf("input expression: " );
-    fgets(cal, sizeof(cal), stdin);
-    
-    length = strlen(cal);
 
     while(1)
     {
+	printf("input expression: " );
+	fgets(cal, sizeof(cal), stdin);
+	length = strlen(cal);
+	
+	cal_num1 = 0;
+	cal_num2 = 0;	
+	count = 0;
+
 	for(i = 0; i < 20; i ++)
 	{
-	    if(cal[i] == 'q')
-	    {
-		printf("end\n");
-		break;		
-	    }   
-	} 
+
+	    if((cal[i] == 'q')&&(length == 2))
+	    {	
+		p++;
+		printf("end\n");			
+	    }	      
+	}
+
+	if(p == 1)
+        break;	
 		 
+
 	for(i = 0; i < 20; i++)
 	{
 	    if(cal[i] == ' ')
@@ -72,19 +82,11 @@ int main()
 		
 		if(count == 2)
 		{
-    		    for(j = i+1 ; j < length-1 ; j++)
+    		    for(j = i + 1 ; j < length - 1 ; j++)
 		    {			
-			num2[j] = cal[j];
+			num2[j - (i + 1)] = cal[j];
+			cal_num2 = atof(num2);
 		    }
-		    for(j =0 ; j < i+1; j ++)
-		    {
-			num2[j] = '0';
-		    }
-		    for( j = length - 1; j < 20; j ++)
-		    {
-			num2[j] = ' ';
-		    }
-		    cal_num2 = atof(num2);
 		}
 	    } 
 
@@ -92,40 +94,74 @@ int main()
 	    {
 		op = cal[i];		
 	    } 
-	    
 	}
-	printf("result : ");
 
+	if(((cal_num1 < -100000) || (cal_num1 > 100000)) || ((cal_num2 < -100000) || (cal_num2 > 100000)))
+	{
+	    printf("wrong numbers\n");
+	    continue;	   
+	}
+	
+	//printf("%.3f %.3f\n", cal_num1, cal_num2);	
 	if(op == ( '+' ))
 	{
 	    float result = add(cal_num1, cal_num2);
-	    printf("%.3f\n", result);	    
+	
+	    if( add(cal_num1, cal_num2) == (int)result)
+	    {
+		printf("result : %d\n", (int)result);
+	    }
+	    else
+	    {
+		printf("result : %.3f\n", result);
+	    }
 	} 
 
 	else if(op == ( '-' ))
         {
             float result = sub(cal_num1, cal_num2);
-	    printf("%.3f\n", result);	    	    
+	    if( sub(cal_num1, cal_num2) == (int)result)
+            {
+                printf("result : %d\n", (int)result);
+            }
+            else
+            {
+                printf("result : %.3f\n", result);
+            }
+	    	    	    
         } 
 
 	else if(op == ( '*' ))
         {
             float result = mul(cal_num1, cal_num2);
-	    printf("%.3f\n", result);
-	   	    
+	    if( mul(cal_num1, cal_num2) == (int)result)
+            {
+                printf("result : %d\n", (int)result);
+            }
+            else
+            {
+                printf("result : %.3f\n", result);
+            }    
         } 
 
 	else if(op == ( '/' ))
         {
             float result = divide(cal_num1, cal_num2);
-	    printf("%.3f\n", result);
-	    	    
+	    if( divide(cal_num1, cal_num2) == (int)result)
+            {
+                printf("result : %d\n", (int)result);
+            }
+            else
+            {
+                printf("result : %.3f\n", result);
+            } 	    
 	}
 	else 
 	{
 	    printf ("error\n");
 	    
-	} break;	
+	} 
+	continue;	
     }  
     return 0;	
 }
