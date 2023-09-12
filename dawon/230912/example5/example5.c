5fa4e6a737e89f995c33c33b71f3ef774b9eebe1
dwlee331 <dawon@zaram.com>
2023-09-12 18:36:50 +0900

    example5

--- dawon/230912/example5/example5.c
+++ dawon/230912/example5/example5.c
@@ -1,20 +1,68 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
     int *p;
-    int i=0, j=0, n=0;
+    int i=0, j=0, n=0, k=1;
+    int num[20]={0, };
+    char ch;
 
     printf("input N: ");
     scanf("%d",&n);
+    while((ch = getchar()) != '\n' && ch != EOF);
     printf("num: ");
 
     p=(int*)malloc(sizeof(int) * n);
+    if(p == NULL)
+    {
+	printf("error");
+	exit(0);
+    }
     for(i = 0; i < n; i++)
     {
         scanf("%d",p+i);
     }
     printf("\n");
-    
 
+    for(i = 0; i < n; i++)
+        printf("%d",*(p+i));  
+   
+    num[0]=*p;
+    
+    for(i = 1; i < n; i++)
+    {
+	for(j = 0; j < i; j++)
+	{
+	    if(*(p+i) != *(p+j))
+	    {
+		num[k] = *(p+i);
+		k++;
+	    }
+	    else 
+		continue;
+	}
+    }
+
+    for(i=0;i<=k;k++)
+	printf("%d",num[k]);
+    printf("\n");
+
+    for(i = 0; i < k-1; i++)
+    {
+	for(j = i+1; j < k; j++)
+	{
+	   printf("(%d, %d) ",num[i],num[j]);
+	}
+    }
+
+    return 0;
+}    
+
+
+
+
+
+
+
+    
 
