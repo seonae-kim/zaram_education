81f35dc0a860d5f74fcf0819e5deb3e6200d3427
hanbeomm <hanbeom@zaram.com>
2023-09-12 14:43:25 +0900

    0912-3

--- hanbeom/230911/personal/practice2/practice2_2.c
+++ hanbeom/230911/personal/practice2/practice2_2.c
@@ -7,83 +7,35 @@ int main()
 {
     char str[500];
     int i =0;
-    double num[2] = {0, 0};
+    char num[6] = {0,};
     
     printf("input number and op ex(43 + 27): ");
     fgets(str, sizeof(str), stdin);	//input the chracter. ex) 4 + 3
     
-    char *token = strtok(str, "+");  // blank	    43 + 27
+    char *token = strtok(str, " ");  // blank	    43 + 27
     while (token != NULL)
     {
-	num[i] = atof(token);
-	printf("%lf\n", num[i]);
-	token = strtok(NULL, "+");
-        i++;	
-    }
-
-    printf("%lf", num[0] + num[1]);    
-    //printf("%f", num2);
-    
-    /*
-    while (token != NULL)	    // address = null = 0 
-    {
-	if
-    */
-    
-    /*
-    for(i =0; i < len; i++)	// + 
-    {
-	str =  	
-    }
-    */
-   // printf("%s", str);		
-
-   // printf("%d", (int)num1);	    //front number
-    
-
-       
-
-   // for(i = 0; str[i];		    //  43 + 54
-    
-/*
-    for(i = 0; i < len; i++)	// if find '+', define operator = '+'
-    {
-        switch(str[i])
-	{
-	    case '+' :
-	    operator = '+';		 
-	    break;   
-		
-	    case '-' :
-	    operator = '-';
-	    break;
-
-	    case '*' :
-	    operator = '*';
-	    break;
+	num[i] = atof(token);		    //  
 	
-	    case '/' :
-	    operator = '/';
-	    break;  
+	token = strtok(NULL, " ");
+	i++;
+	/*
+	if(num[1] == '+')
+	{
+	    //num[1] = token[0];
+	    token = strtok(NULL, " ");
+	    i++;
 	}
-    }
-    
-    switch(operator)
-    {
-        case '+' :
-	    result = sum(str);
-	    break;
-	case '-' :
-	    result = minus(str);
-	    break;
-	case '*' :
-	    result = multiple(str);
-	    break;
-	case '/' :
-	    result = divide(str);
-	    break;    
-    }
-*/
 
-   // printf("%d\n",result);
+	else
+	{
+	    token = strtok(NULL, " ");
+	    i++;
+	}
+	*/
+    }
+      printf("%d ", num[0]);		// -27
+      printf("%c ", num[1]);		// blank
+      printf("%d ", num[2]);		// -43
 }
+
