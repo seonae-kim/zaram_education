1db6c8ca9a48140671f4f11393ac5662d8a70416
hanbeomm <hanbeom@zaram.com>
2023-09-12 09:55:29 +0900

    0912

---
+++ hanbeom/230911/personal/practice2/practice2_2.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <string.h>
+
+int sum(char ch[])
+{
+    int answer = 0;
+    int len = strlen(ch);
+    int i;
+    //printf("sum");
+    for(i = 0; i < len; i++)
+	{
+	    if(ch[i] > '0' && ch[i] < '9')
+		answer += (ch[i] - 48);	//real number
+	}
+    return answer;
+}
+
+int minus(char ch[])
+{
+    int answer = 0;
+    int len = strlen(ch);
+    int i;
+    for(i = 0; i < len; i++)
+     {
+            if(ch[i] > '0' && ch[i] < '9')
+	    {	
+		if(answer == 0)
+		    answer = (ch[i] - 48);    	
+		else
+		    answer -= (ch[i] - 48);
+	    }
+
+     }
+    return answer;
+}
+
+int multiple(char ch[])
+{
+    int answer = 0;
+    int len = strlen(ch);
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        if(ch[i] > '0' && ch[i] < '9')
+	{
+	   if(answer == 0)
+	       answer = (ch[i] - 48);
+           else
+	       answer *= (ch[i] - 48);
+	}
+    }
+    return answer;
+}
+
+int divide(char ch[])
+{
+    int answer = 0;
+    int len = strlen(ch);
+    int i;
+
+    for(i = 0; i < len; i++)
+        {
+	if(ch[i] > '0' && ch[i] < '9')
+        {
+           if(answer == 0)
+               answer = (ch[i] - 48);
+           else
+               answer /= (ch[i] - 48);
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    char str[500];
+    char operator = 'a';
+    int result = 0;
+    int i;
+    
+    printf("input real number: ");
+    fgets(str, sizeof(str), stdin);	//input the chracter. ex) 4 + 3
+
+    double num1 = atof(str);     //change string to real number  
+    double num2 = atof(str);     //change string to real number 
+ 
+    int len = strlen(str);
+
+				
+    for(i = 0; str[i]		    //
+
+    for(i = 0; i < len; i++)	// if find '+', define operator = '+'
+    {
+        switch(str[i])
+	{
+	    case '+' :
+	    operator = '+';		 
+	    break;   
+		
+	    case '-' :
+	    operator = '-';
+	    break;
+
+	    case '*' :
+	    operator = '*';
+	    break;
+	
+	    case '/' :
+	    operator = '/';
+	    break;  
+	}
+    }
+    
+    switch(operator)
+    {
+        case '+' :
+	    result = sum(str);
+	    break;
+	case '-' :
+	    result = minus(str);
+	    break;
+	case '*' :
+	    result = multiple(str);
+	    break;
+	case '/' :
+	    result = divide(str);
+	    break;    
+    }
+  
+
+    printf("%d\n",result);
+}
