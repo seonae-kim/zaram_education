17fb044a79212761dd7ff29ec98fdaef908871bd
dongjun <yangdj@zaram.com>
2023-09-13 18:21:44 +0900

    0912 study - example4 solvedgit statusgit status!

--- ydj/study/0912/example4/4_prob.c
+++ ydj/study/0912/example4/4_prob.c
@@ -4,13 +4,14 @@ int main()
 {
 	int N = 0;
 	int i = 0, j = 0;
-	int count = 0, max = 0, num = 0;
+	int count = 0, max = 0, num = 0, arr_max =0, arr_i = 0;
 	int flag = 0;
 	printf("Please enter the N : ");
 	scanf("%d", &N);
 	printf("Please enter the num : ");
 
 	int arr[N];
+	int arr_count[N];
 
 	for (i = 0; i < N; i++)
 	{
@@ -19,35 +20,47 @@ int main()
 	
 	for (i = 0; i < N; i++)
 	{
-		count = 0;
-		for (j = 0 ; j < N; j++)
+		count = 1;
+		for (j = i + 1 ; j < N; j++)
 		{
 			if (arr[i] == arr[j])
 			{
 				count++;
 			}
 		}
+		arr_count[i] = count;
 
 		if (max < count)
 		{
 			num = arr[i];
 			max = count;
 		}
+	}
 
-		else if (max == count)
+	for(i = 0; i < N; i++)
+	{
+		if (arr_count[i] > arr_max)
 		{
-			if ((i == N - 1) && (num != arr[N - 1]))
-			{
-				printf("%d \n", -1);
-				flag++;
-				break;
-			}
+			arr_max = arr_count[i];
 		}
 	}
-	
-	if (flag == 0)
+
+	for (i = 0; i < N; i++)
+	{
+		if(arr_max == arr_count[i])
+		{
+			flag++;
+		}
+	}
+
+	if (flag == 1)
 	{
 		printf("%d \n", num);
 	}
+	else
+	{
+		printf("-1 \n");
+	}
+
 	return 0;		
 }
