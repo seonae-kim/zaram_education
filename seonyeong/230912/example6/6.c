40b3450a521f35bb03d369c49138117615325451
seonyeong2 <sykwak@zaram.com>
2023-09-14 10:28:55 +0900

    0912-ex6

--- seonyeong/230912/example6/6.c
+++ seonyeong/230912/example6/6.c
@@ -8,7 +8,6 @@ int main()
     char arr[100] = { 0 };
     int length = 0;
     int numbers[100] = { 0 };
-    int count = 0;
     char num_arr[10] = { 0 };
     int num_idx = 0;
     int min_idx = 0;
@@ -27,42 +26,41 @@ int main()
     {
         if (arr[i] >= '0' && arr[i] <= '9')
         {
-            if (arr[i + 1] >= '0' && arr[i + 1] <= '9')
+			num_idx = 0;
+            char num_arr[10] = { 0 };
+			if (arr[i + 1] >= '0' && arr[i + 1] <= '9')
             {
                 while (arr[i] >= '0' && arr[i] <= '9')
                 {
                     num_arr[num_idx++] = arr[i++];
-		    if (arr[i] > '9' && '0' > arr[i])
-			break;
+					if (arr[i+1] > '9' && '0' > arr[i+1])
+					break;
                 }
 
                 num = atof(num_arr);
                 numbers[p++] = num;
-		arr[i+1] = 'a';
-            }
+			}
             else
             {
                 numbers[p++] = arr[i] - '0';
-		count = 0;
+				arr[i] = 'z';
             }
-
-	    for (j = 0; j < i; j++)
+		}
+	}
+			
+	for(i =0; i < length; i++)
+	{
+		for (j = 0; j < i; j++)
+        {
+            if (numbers[j] == numbers[i])
             {
-                if (numbers[j] == num)
-                {
-                    count = 1;
-                    break;
-                }
-            }
-
-            if (count != 1)
-            {
-		numbers[j++] = num;
+				numbers[j] = '\0';
+                break;
             }
         }
-    }
+	}   
     
-    for (i = 0; i < p - 1; i++)
+	for (i = 0; i < p - 1; i++)
     {
         min_idx = i;
 
@@ -93,101 +91,3 @@ int main()
         return 0;
 
 }
-
-
-
-/*
-int main()
-{
-    char arr[50] = {0};
-    int length = 0;
-    char new_arr[50] = {0};
-    int i = 0; 
-    int j = 0;
-    int a = 0;
-    int p = 0;
-    char num_arr[5] = {0};
-    int num = 0;
-    int count = 0;
-    int min_num = 0;
-    int x = 0;
-    int h = 0;
-
-    printf("input:");
-    fgets(arr, sizeof(arr),stdin);
-    length = strlen(arr);
-
-    printf("output: ");
-
-    
-    for (i = 0; i < length; i++)
-    {
-	    if (arr[i] >= '0' && arr[i] <= '9')
-         {
-             p = i;
-             for(a = i + 1; a < length; a++)
-             {
-                 if(arr[a] >= '0' && arr[a] <= '9')
-                 {
-                     p++;
-                 }
-                 else
-                 break;
-             }
-
-             if(p == i)
-             {
-                 num_arr[h] = arr[i];
-                 count = 0;
-             }
-             else
-             {
-                 for(j = 0; j <= p; j++)
-                 {
-                     num_arr[h] = arr[j];
-                     h++;
-                     count = 1;
-                 }
-             }
-
-             for( j = 0; j < h; j++)
-             {
-                 printf("%c", num_arr[j]);
-             }
-             printf("\n");
-
-          }
-          num_arr[0] = 'z';
-          h=0;
-      }
-
-
-
-	for (i = 0; i < length ; i++)
-	{
-	    min_num = i;
-
-	    for ( j = i + 1; j < length; j++)
-	    {
-		 if (new_arr[j] < new_arr[min_num])
-		{
-		     min_num= j;
-		}
-	    }
-
-	    x = new_arr[i];
-	    new_arr[i]= new_arr[min_num];
-	    new_arr[min_num]= x;
-	}
-
-	for(i= 0; i< length ;i++)
-	{
-	    if( new_arr[i] != 0)
-	    {
-		printf("%d ",new_arr[i]);
-	    }
-	}
-    
-    printf("\n");
-        return 0; 
-}*/
