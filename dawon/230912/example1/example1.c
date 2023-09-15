6550e01141f02e2d37cab3116d3894fb83debea7
dwlee331 <dawon@zaram.com>
2023-09-15 10:17:04 +0900

    example1

--- dawon/230912/example1/example1.c
+++ dawon/230912/example1/example1.c
@@ -1,127 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main()
 {
-    char array[5][20]={0, };
-    char array2[5][20]={0, };
-    char string[30] = {0, };
-    int cmp[10]={0, };
-    int i=0, j=0, k=0, n=0, p=1, m;
-    int len=100;
-    int index=0, index2=0;
-    printf("input:" );
-
-    for(i = 0; i < 5; i++)
-    {
-       scanf("%s",array[i]);
-    }
-
-	for(i = 0; i < 5; i++)
-	{
-		cmp[i]=strlen(array[i]);
-	}
-
-/*	for(i = 0; i < 5; i++)
-	{
-		if(cmp[i] < len)
-		{
-			len=cmp[i];
-			index2=i;
-		}
-	}
-*/
-	for(i = 0; i < 5 ; i++)
-	{
-	/*	if(i == index2)
-			continue;
-	*/		
-		if(cmp[i] > len)
-		{
-			strcpy(array2[i],array[i]);
-			
-		}
-		if(cmp[i] == len)
-		{
-			while(cmp[i-p] == len) 
-			{
-				for(n = 0; n < 10 ; n++)
-				{
-					if(strcmp(array[i-n],array2[i-p]) == -1)
-					{
-						strcpy(string,array2[i-p]);
-						strcpy(array2[i-p],array[i-n]);
-						strcpy(array2[i-n],string);
-						p++;
-					}
-
-
-				/*	else if(strcmp(array[i-n],array2[i-p]) == 1)
-					{
-						strcpy(array2[i],array[i]);
-				
-					}
-					*/
-				}		
-			}
-			p=1;
-		}
-		if(cmp[i] < len)
-		{
-			p=0;
-			if(i==0)
-			{
-				strcpy(array2[i],array[i]);
-				continue;
-			}
-
-			k=i-1;
-			while(cmp[k] == len)
-			{
-				k--;
-			}
-			for(n = 0; n < i-k ; n++)
-			{
-					strcpy(array2[i-p],array2[i-n]);
-					p++;
-			}
-		    strcpy(array2[k],array[i]);
-			if(strlen(array2[k]) == strlen(array2[k-1]))
-			{
-				m=1;
-				while(strlen(array2[k-m]) == strlen(array2[k]))
-				{
-					for(n = 0; n < 10 ; n++)
-					{
-						if(strcmp(array2[k],array2[k-m]) == -1)
-						{
-							strcpy(string,array2[k-m]);
-							strcpy(array2[k-m],array[k-n]);
-							strcpy(array2[k-n],string);
-							m++;
-						}
-
-					}
-				
-
-
-				}
-			}		
+   char array[5][20] = { 0, };
+   char array2[5][20] = { 0, };
+   char string[20] = { 0., };
+   int n = 100;
+   int index;
 	
-		}
-		len=cmp[i];
-	}
-	
-	for(i = 0; i < 5; i++)
-	{
-		for(j = 0; j < 20; j++)
-		{
-			printf("%c",array2[i][j]);
-		}
-		printf("\n");
-	}
+   printf("input: ");
+   for (int i = 0; i < 5; i++)
+   {
+      scanf("%s", array[i]);
+   }
 
-	printf("\n");    
-        
-    return 0;
+
+   for (int j = 0; j < 5; j++)
+   {
+      for (int i = 0; i < 5; i++)
+      {
+         if (strlen(array[i]) < n)
+         {
+            n = strlen(array[i]);
+            index = i;
+         }
+         if (strlen(array[i]) == n)
+         {
+            index = i;
+         }
+
+      }
+
+      strcpy(array2[j], array[index]);
+      memset(array[index], '1', sizeof(char) * 19);
+      n = 100;
+
+   }
+   printf("\n");
+   n = 1;
+   int k = 0;
+   int i = 0;
+
+   for (i; i < 5; i=i+k)
+   {
+      if (strlen(array2[i]) == strlen(array2[i + 1]))
+      {
+		  k=0;
+         while (strlen(array2[i]) == strlen(array2[i + n]))
+         {
+            k++;
+            n++;
+         }
+         for (int j = i; j < i+k; j++)
+         {
+            for (int m = j+1; m <= i+k; m++)
+            {
+               if (strcmp(array2[m], array2[j]) < 0)
+               {
+                  strcpy(string, array2[j]);
+                  strcpy(array2[j], array2[m]);
+                  strcpy(array2[m], string);
+               }
+            }
+         }
+		 k=k+1;
+      }
+	  else 
+		  k=1;
+   }
+   printf("output: ");
+   for (int i = 0; i < 5; i++)
+   {
+      for (int j = 0; j < 5; j++)
+      {
+         printf("%c", array2[i][j]);
+      }
+      printf(" ");
+   }
+
+	printf("\n");
+   return 0;
 }
+
