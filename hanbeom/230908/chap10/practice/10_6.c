a21960ca256adc9570d3b6a7147570852ced828c
hanbeomm <hanbeom@zaram.com>
2023-09-18 17:17:46 +0900

    0918_4

--- hanbeom/230908/chap10/practice/10_6.c
+++ hanbeom/230908/chap10/practice/10_6.c
@@ -1,7 +1,70 @@
 #include <stdio.h>
 
-void SwapArray(int*p1 *p2 size);
+void SwapArray(int*p1, int*p2, int size);
 
 int main()
 {
+	int a[5] = {1, 3, 5, 7, 9};
+	int b[5] = {2, 4, 6, 8, 10};
+	int i;
 	
+	printf("before swap\n");
+	
+	printf("a: ");
+	for(i =0; i < 5; i++)
+	{	
+		printf("%d ", a[i]);	
+	}
+
+	printf("b: ");
+	for(i =0; i < 5; i++)
+	{	
+		printf("%d ", b[i]);	
+	}
+
+	printf("\n");
+
+	SwapArray(a, b, 5);
+	
+	printf("after swap\n");
+	
+	printf("a: ");
+	for(i =0; i < 5; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	
+	printf("b: ");
+	for(i =0; i < 5; i++)
+	{
+		printf("%d ", b[i]);
+	}
+
+}
+
+void SwapArray(int*p1, int*p2, int size)	//use pointer only here
+{
+	/*
+	int temp;
+	temp = *p2;
+	*p2 = *p1;
+	*p1 = temp;
+	*/
+	
+	/*
+	int* temp;
+	temp = p1;
+	p2 = p1;			
+	p1 = temp;
+	*/
+
+	int temp =0;
+	int i;
+	for(i = 0 ; i < size; i++)
+	{
+		temp = p1[i];
+		p1[i] = p2[i];
+		p2[i] = temp;
+	}
+
+}
