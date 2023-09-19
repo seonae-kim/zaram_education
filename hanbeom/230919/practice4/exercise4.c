2a153e7fa011a8aeb853952d92a415d3eae8d157
hanbeomm <hanbeom@zaram.com>
2023-09-19 15:35:48 +0900

    0919_prac4

---
+++ hanbeom/230919/practice4/exercise4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	int i;
+	int num[100] ={0,};
+	int num1;
+	int num2;
+
+	printf("input N: \n");
+	scanf("%d", &n);
+	
+	printf("input many number: \n");
+	for(i =0; i < n; i++)
+	{
+		scanf("%d", &num[i]);
+	}
+
+	printf("input num1 \n");
+	scanf("%d", &num1);
+
+	printf("input num2 \n");
+	scanf("%d", &num2);
+
+	for(i = num1; i <= num2; i++)
+	{
+		printf("%d ", num[i]);
+	}
+}
