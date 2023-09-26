a90dc966e71d50eb464686c0c1d17e2b2296dcc8
hanbeomm <hanbeom@zaram.com>
2023-09-26 18:19:48 +0900

    79,81

---
+++ hanbeom/230926/practice79/79.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t currentTime;
+    struct tm* timeInfo;
+
+    time(&currentTime);
+
+    timeInfo = localtime(&currentTime);
+
+    printf("currenttime: %dy %dm %dd %dh %dm %ds\n",
+        timeInfo->tm_year + 1900,  
+        timeInfo->tm_mon + 1,      
+        timeInfo->tm_mday,         
+        timeInfo->tm_hour,         
+        timeInfo->tm_min,          
+        timeInfo->tm_sec);         
+
+    return 0;
+}
---
+++ hanbeom/230926/practice81/81.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <time.h>
+
+int main() 
+{
+    FILE* file;
+    time_t currentTime;
+    struct tm* timeInfo;
+    char timeString[80]; 
+
+    
+    time(&currentTime);
+
+    timeInfo = localtime(&currentTime);
+
+    strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", timeInfo);
+    file = fopen("current_time.txt", "a");
+
+    if (file == NULL) {
+        printf("can't open file.\n");
+        return 1;
+    }
+    
+    fprintf(file, "current time: %s\n", timeString);
+    fclose(file);
+
+    printf("Success\n");
+
+    return 0;
+}
