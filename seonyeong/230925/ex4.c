33591b2144d2c7a81b70cdf62c21577a3f743663
seonyeong2 <sykwak@zaram.com>
2023-09-26 16:47:14 +0900

    ex4

--- seonyeong/230925/ex4.c
+++ seonyeong/230925/ex4.c
@@ -3,29 +3,53 @@
 int main()
 {
 	int N = 0;
+	int a = 0, temp = 0;
 	int i = 0, j = 0, num = 1;
+	int idx = 1;
 	int arr[20][20] = {0};
 
 	printf(" input N:");
 	scanf("%d", &N);
 
-	
-	while(1)
+	temp = N;
+	for( idx = 0; idx < N - 1; idx++ )
 	{
-		for( j = 0; j < N; j++)
+		if( num == N * N + 1)
+			break;
+
+		if(idx >= 1)
 		{
-			arr[0][j] = num;
-			num++;
+			a = a + 1;
+			temp = temp - 1;
+		}
+		
+		
+		for( j = a; j < temp; j++)
+		{
+			i = a;
+			arr[i][j] = num++;
 		}
 	
-		for( i = 1; i < N; i++)
+		num--;
+		for(i = a; i < temp; i++)
 		{
-			arr[i][N-1] = num;
-			num++;
+			j = temp-1;
+			arr[i][j] = num++;
 		}
+		
+		num--;
+		for(j = temp-1; j >= a; j--)
+		{
+			i = temp-1;
+			arr[i][j] = num++;
+		}
+		num--;
 
-
-
+		for( i = temp-1; i > a; i--)
+		{
+			j = a;
+			arr[i][j] = num++;
+		}
 	}
 
 
@@ -35,14 +59,23 @@ int main()
 
 
 
-		for(i = 0; i < N; i++)
+
+
+
+
+
+
+
+
+
+	for(i = 0; i < N; i++)
+	{
+		for(j = 0; j < N; j++)
 		{
-			for(j = 0; j < N; j++)
-			{
-				printf("%d ", arr[i][j]);
-			}
-			printf("\n");
+			printf("%2d ", arr[i][j]);
 		}
+		printf("\n");
+	}
 	
 	
 	return 0;
