65d3c8ed78b7592cf1ed9c725ddae085883a716e
dwlee331 <dawon@zaram.com>
2023-09-26 14:15:21 +0900

    230925

--- dawon/230925/example2/example2.c
+++ dawon/230925/example2/example2.c
@@ -2,35 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+int recur(int n);
 int main()
 {
 	char num[10]={0, };
 	int num_i=0;
 	int sum=0;
+	char num_s[10]={0, };
 	int i = 0, j = 0, h = 0;
-	int a = 0, b = 0, c = 0;
 	int d = 0;
 
-
 	printf("input: ");
-	scanf("%d",&d);
-	num_i = atoi(num);
-
-	for(i = 0; i < 10; i++)
+	scanf("%d",&num_i);
+	
+	for(i = 0; i < 2000; i++)
 	{
-		for(j = 0; j < 10; j++)
+		if(i + recur(i) == num_i) 
 		{
-			for(h = 0; h < 10; h++)
-			{
-				sprintf(num,"%d%d%d",i,j,h);
-				num_i = atoi(num);
-				if(i + j + h + num_i == d)
-				{
-					printf("output: %d\n",num_i);
-				}
-			}
+			printf("\noutput: %d\n",i);
 		}
 	}
-
 	return 0;
 }
+
+int recur(int n)
+{
+	if(n % 10 == n)
+	{
+		return n;
+	}
+
+	return recur(n / 10) + (n % 10);
+}
+
+
+
+
+
