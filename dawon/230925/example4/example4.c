2142985e09debb77e8155fe2c71cc5a713c53d9c
dwlee331 <dawon@zaram.com>
2023-10-04 10:25:40 +0900

    ex4

--- dawon/230925/example4/example4.c
+++ dawon/230925/example4/example4.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-	int n = 0, i = 0, j = 0;
+	int n = 0, i = 0, j = 0, m=0,l=0;
 	int **arr1;
-	int **arr2;
 	int a = 0;
+	int k = 1;
+	int b = 1;
+	int p = 1;
 
 	printf("num: ");
 	scanf("%d",&n);
@@ -18,35 +20,49 @@ int main()
 		*(arr1+i) = (int*)malloc(sizeof(int)*n);
 	}
 
+	while(p != 26)	
+	{
+			for(j = a; j < n; j++)
+			{
+				*(*(arr1 + a) + j) = p++;
+			}
+
+			for(k = a+1; k < n; k++)
+			{				
+				*(*(arr1 + k) + j) = p++;
+			
+			}
+
+			for(l = n-2; l <= a; l--)
+			{
+					
+				*(*(arr1 + k) + l) = p++;
+				
+			}
+			
+			for(m = n-2; m < a; m--)
+			{
+				*(*(arr1 + m) + l) = p++;
+			}	
+
+				
+		a++;
+		n--;
+	}
+
+
 	for(i = 0; i < n; i++)
 	{
 		for(j = 0; j < n; j++)
 		{
-			scanf("%d",*(arr1+i)+j);
+			printf("%d ",*(*(arr1+i)+j));
 		}
+		printf("\n");
 	}
 
-	for(i = 0; i < n; i++)
-	{
-		arr2[0][i] = arr1[0][i];
-	}
-	
-	for(i = 1; i < n; i++)
-	{
-		arr2[i][2] = arr1[1][a];
-		a++;
-	}
-	for(i = n-1; i > 0; i--)
-	{
-		arr2[i][n] = arr1
-	
-
-
-
-
-
-
-
-		
 	return 0;
 }
+
+
+
+
