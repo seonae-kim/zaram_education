69b8f4e981c2b4b4a1f7b20d5139e31ea2a28eaa
dwlee331 <dawon@zaram.com>
2023-10-04 17:51:24 +0900

    231004/

---
+++ dawon/231004/thread.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+void *rannum();
+
+void *rannum()
+{
+	int random[5] = {0, };
+	int i = 0, j = 0;
+	pthread_t tid;
+	
+	srand(time(NULL));
+	
+	while(1)
+	{
+		
+		for(i = 0; i < 5; i++)
+		{
+			random[i] = rand() % 9;
+			for(j = 0; j < i; j++)
+			{
+				if(random[i] == random[j])
+				{
+					i--;
+					break;
+				}
+			}
+		}
+
+		for(j = 0; j < 5; j++)
+		{
+			for(i = 0; i < 5; i++)
+			{
+				printf("%d ",random[i]);
+			}
+			printf("\n");
+		}
+
+		sleep(1);
+		sleep(1);
+		sleep(1);
+	}
+
+}
+
+int main()
+{
+	pthread_t tid;
+	int t,i;
+	char a;
+	void **re;
+	t=pthread_create(&tid,NULL,rannum,NULL);
+
+	
+	scanf("%c",&a);
+
+	while(a != 'q')
+	{	
+			if(a != 'q')
+			{
+				printf("input : %c\n",a);
+				printf("output: %c\n",a);
+			}
+
+			scanf("%c",&a);
+			
+	}
+
+	return 0;
+}
+
+
+
