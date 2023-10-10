9ec75be8025c090a055beb0fb0e8089740efd77e
dongjun <yangdj@zaram.com>
2023-10-10 13:08:38 +0900

    0927 update

---
+++ ydj/study/0927/example81/81_prob.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<time.h>
+
+int main()
+{
+	struct tm *t;
+	time_t now = time(NULL);
+
+	char now_time[20];
+
+	strftime(now_time, sizeof(now_time), "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+	FILE *fp;
+	fp = fopen("time.txt", "a+");
+	if (fp == NULL)
+	{
+		printf("Fail to open the file, Please try again");
+	}
+	fprintf(fp, "%s", now_time);
+
+	printf("DONE! Please check the file !\n");
+	return 0;
+}
