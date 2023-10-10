c6c6cc5b2d70cfcf89b0714081ee9ebeaf7d6159
junhee <junhee@zaram.com>
2023-10-10 19:29:22 +0900

    modify example2.c/add example2_other.c

--- junhee/231005/example2.c
+++ junhee/231005/example2.c
@@ -2,36 +2,70 @@
 숫자 + 각자리의 숫자의 합이다.
 ex) input : 216, output : 198
 */
+
 #include <stdio.h>
+
+int cal (int n, int x)
+{
+	n /= 10; 	
+	if(n <= 0)
+	{
+		return x + 1;
+	}
+	
+	return cal(n , x + 1);
+}
+
+int rsum(int n, int len, int x)
+{
+	int i;
+	int nums[50];
+	int rst = 0;
+	int rst_1 = 0;
+	int rst_2 = 0;
+	int sum = 0;
+	int a = 1;
+	
+	for(i = 0; i < len; i++)
+	{
+		a *= 10; 
+	}
+	
+	for (i = 0; i < len; i++)
+	{
+		rst_1 = x % a;
+		a /= 10;
+		rst_2 = rst_1 / a;
+		nums[i] = rst_2;
+	}	
+	
+	for ( i = 0; i < len; i++)
+	{
+		sum += nums[i];
+	}
+	
+	rst = x + sum;
+	
+	if(rst == n)
+	{
+		return x;
+	}
+	
+	return rsum(n, len, x + 1);
+}
+
 
 int main()
 {
-	int i,j,k,o;
 	int num = 0;
 	int rst = 0;
-	int tmp = 0;
+	int len = 0;
+	int x = 0;
 	scanf("%d", &num);
-	for(k = 0; k < 10; k++)
-	{
-		for(i = 0; i < 10; i++)
-		{
-			for( j = 0; j < 10; j++)
-			{
-				for (o = 0; o < 10; o++)
-				{
-					tmp = (k*1000) + (i*100) + (j*10) + o + k + i + j +o;
-					if(num == tmp)
-					{
-						rst = (k*1000) + (i*100) + (j*10) + o;
-					}
-				}
-			}
-		}	
-	}
-	
-	
-	printf("%d",rst);
+
+	len = cal(num, x);
+	rst = rsum(num, len, x);
+	printf("%d", rst);
 	
 	return 0;
 }
-
