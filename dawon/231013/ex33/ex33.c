34dc3a22dfde4d341dcfed0ab809957e6a1c984d
dwlee331 <143775667+dwlee331@users.noreply.github.com>
2023-10-16 09:59:21 +0900

    Update ex33.c

--- dawon/231013/ex33/ex33.c
+++ dawon/231013/ex33/ex33.c
@@ -6,17 +6,10 @@ int main()
 {
 	char mystring[20];
 	int i = 0;
-//	int n = 0;
-
-//	printf("n: ");
-//	scanf("%d",&n);
-
-//	mystring = (int *)malloc(sizeof(int) * n);
 
 	printf("string: ");
 	fgets(mystring,sizeof(mystring),stdin);
 	mystring[strlen(mystring) - 1] = '\0';
-//	scanf("%s",mystring);
 
 	for(i = 0; i < strlen(mystring); i++)
 	{
