41f02a8c277339a9ff108b27581af34610adbf3c
dwlee331 <dawon@zaram.com>
2023-10-18 18:08:57 +0900

    ex72

--- dawon/231013/ex72/ex72.c
+++ dawon/231013/ex72/ex72.c
@@ -6,36 +6,35 @@
 int main()
 {
 	int n =0;
-	char *arr;
+	char **arr;
 	int max = 0, count = 0, i = 0, j = 0, maxcount = 0;
 
 	printf("n: ");
 	scanf("%d",&n);
 
-	arr = (char*)malloc(sizeof(char) * n);
+	arr = (char**)malloc(sizeof(char*) * n);
+	for(i = 0; i < n; i++)
+	{
+		*(arr + i) = (char*)malloc(sizeof(char) * n);
+	}
 
 	for(i = 0; i < n; i++)
 	{
-		scanf("%s",arr + i);
-		printf("strlen: %d\n",strlen(arr + i));
-		if(strlen(arr + i) > max)
-			max = strlen(arr + i);
+		scanf("%s",*(arr + i));
+		if(strlen(*(arr + i)) > max)
+			max = strlen(*(arr + i));
 	}
 
-	printf("max: %d\n",max);
-	printf("max: %s\n",arr);
 
 	for(i = 1; i <= max; i++)
 	{
 		for(j = 0; j < n; j++)
 		{
-			printf("strlen: %d\n",strlen(arr + j));
-			if(strlen(arr + j) == i)
+			if(strlen(*(arr + j)) == i)
 			{
 				count++;
 			}
 		}
-		printf("%d count: %d\n",i,count);
 		if(count > maxcount)
 		{
 			maxcount = count;
