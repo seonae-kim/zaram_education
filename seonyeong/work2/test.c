cdf2049a41c8cf094e8b6038f0d1718ecf2ce9cb
jhpark <jhpark@zaram.com>
2023-10-27 17:20:12 +0900

    test

---
+++ seonyeong/work2/test.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int j = 0;
+	int num[4][4] = { 0 };
+	int a = 0, b = 0;
+	int sum = 0, max = 0;
+
+	printf("8 numbers: ");
+
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j ++)
+		{
+			scanf("%d",&num[i][j]);
+
+		}
+	}
+	printf("%d\n", num[0][0]);
+	printf("Test Code \n");
+
+	for(i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+		  printf("###\n");
+			for (a = 0; a < 4; a++)
+			{
+				for (b = 0; b < 4; b++)
+				{
+					if ( i  == j ) continue;
+					if ( j == a ) continue;
+					if ( a == b ) continue;
+
+						sum = num[0][i] + num[1][j] +  num[2][a] + num[3][b];
+						if (sum > max)
+						{
+
+							max = sum;
+						}
+						//printf("(%d %d %d %d)",num[0][i], num[1][j], num[2][a], num[3][b]);
+				}
+			}
+			
+		}
+	}
+	printf("%d", max);
+		
+	printf("\n");
+
+	printf("exit\n");
+
+}
+
+
+
+
